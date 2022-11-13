3cb9c02a1f72b44caf6562918076ce2054c13e5c
FooQoo <horseshoes.buzz.17@gmail.com>
2022-11-13 19:30:08 +0900

    fix lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,11 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `eslint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "*.{css,scss}": ["yarn stylelint:fix"],
   "*": ["yarn format"],
 };
